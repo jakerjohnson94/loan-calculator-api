ddcb98f071544d2bad49d7507de01e7716f9425a
tgentry300 <taylor.gentry300@gmail.com>
2018-11-04 18:13:41 -0500

    init, mailing not working

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'jacobrichardjohnson94@example.com',
+  from: 'yoMama',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
+app.listen(3021, () => console.log('Example app listening on port 3021!'));
