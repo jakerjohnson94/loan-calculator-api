3cd91650d59f684fe21ac1e2d9d7aa398e3eea61
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-11-04 18:31:48 -0500

    email test is working

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
 
-
 app.use(express.json());
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'jacobrichardjohnson94@example.com',
-  from: 'yoMama',
+  to: 'jacobrichardjohnson94@gmail.com',
+  from: 'yoMama@gmail.com',
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>'
 };
 sgMail.send(msg);
 
